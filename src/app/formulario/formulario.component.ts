e99591f28608c736b654e13164b04dee174867e6
Rolando Chirinos <yutipu@gmail.com>
2025-11-01 22:09:04 -0500

    Agregar producto

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular
 import { Producto } from '../producto/producto.model';
 import { FormsModule } from '@angular/forms';
 import { ProductoService } from '../producto.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -11,23 +12,34 @@ import { ProductoService } from '../producto.service';
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
-descripcionInput: string = '';
-precioInput: number | null = null;
-constructor(
-private productoService: ProductoService
-){}
-agregarProducto() {
-// Validación simple para evitar productos sin descripción o con precio cero
-if (this.descripcionInput.trim() === ''
-|| this.precioInput == null || this.precioInput <= 0) {
-console.log('Debe ingresar una descripción y un precio válidos');
-return;
-}
-const producto = new Producto(this.descripcionInput, this.precioInput);
-// Agregamos el nuevo producto usando el servicio
-this.productoService.agregarProducto(producto);
-// Limpia los campos de entrada después de agregar el producto
-this.descripcionInput = '';
-this.precioInput = null;
-}
+  descripcionInput: string = '';
+  precioInput: number | null = null;
+  constructor(
+    private productoService: ProductoService,
+    private router: Router
+  ) { }
+  guardarProducto(evento: Event) {
+    evento.preventDefault();
+    // Validación simple para evitar productos sin descripción o con precio cero
+    if (this.descripcionInput.trim() === ''
+      || this.precioInput == null || this.precioInput <= 0) {
+      console.log('Debe ingresar una descripción y un precio válidos');
+      return;
+    }
+    const producto = new Producto(this.descripcionInput, this.precioInput);
+    // Agregamos el nuevo producto usando el servicio
+    this.productoService.agregarProducto(producto);
+    // Limpia los campos de entrada después de agregar el producto
+    this.descripcionInput = '';
+    this.precioInput = null;
+
+    //para redirigir al inicio
+    this.router.navigate(['/']);
+  }
+
+  cancelar() {
+    //para redirigir al inicio
+    this.router.navigate(['/']);
+  }
+
 }
--- src/app/listado-producto/listado-producto.component.ts
+++ src/app/listado-producto/listado-producto.component.ts
@@ -4,6 +4,7 @@ import { ProductoComponent } from '../producto/producto.component';
 import { FormsModule } from '@angular/forms';
 import { FormularioComponent } from "../formulario/formulario.component";
 import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
 
 
 
@@ -18,9 +19,16 @@ import { ProductoService } from '../producto.service';
 export class ListadoProductoComponent {
   productos: Producto[] = [];
 
-  constructor(private productoService: ProductoService) { }
+  constructor(private productoService: ProductoService, 
+    private router: Router
+  ) { }
   ngOnInit(): void {
     // Inicializamos los productos
     this.productos = this.productoService.productos;
+  }
+
+  agregarProducto() {
+    this.router.navigate(['agregar']);
+  
   }
 }
