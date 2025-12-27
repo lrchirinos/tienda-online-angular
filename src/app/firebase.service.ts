386873a7ba7cac36f5416a2fb90c23b5cfa902a4
Rolando Chirinos <yutipu@gmail.com>
2025-12-27 03:56:39 -0500

    environment para poteger apikey

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 // Import the functions you need from the SDKs you need
+import { environment } from '../environments/environment';
 import { initializeApp } from 'firebase/app';
 import { Auth, getAuth } from 'firebase/auth';
 import { Firestore, getFirestore } from 'firebase/firestore';
@@ -8,20 +9,13 @@ import { Firestore, getFirestore } from 'firebase/firestore';
   providedIn: 'root'
 })
 export class FirebaseService {
-  firebaseConfig = {
-    apiKey: "AIzaSyCYmaCbq4VcBzMzQBDIPeUho92eqP_ZV_8",
-    authDomain: "tienda-online-448cc.firebaseapp.com",
-    databaseURL: "https://tienda-online-448cc-default-rtdb.firebaseio.com",
-    projectId: "tienda-online-448cc",
-    storageBucket: "tienda-online-448cc.firebasestorage.app",
-    messagingSenderId: "872608539450",
-    appId: "1:872608539450:web:4a559ec06a77510a8e6603"
-  };
+  
   public auth: Auth;
   public firebase: Firestore;
 
   constructor() {
-    const app = initializeApp(this.firebaseConfig);
+    // Usa la variable del environment
+    const app = initializeApp(environment.firebaseConfig);
     this.auth = getAuth(app);
     this.firebase = getFirestore(app);
   }
